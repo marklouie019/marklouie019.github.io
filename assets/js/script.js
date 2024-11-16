1d047ca3c12fa4aa33b33d800bacc1f317eda591
Fushiguro019 <marklouievillanueva584@gmail.com>
2024-11-16 11:14:43 +0800

    Fixed animations, hover fx, & menu bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,33 +5,27 @@ document.addEventListener('mousemove', (e) => {
     cursor.style.top = e.clientY + 'px';
 });
 
-const links = document.querySelectorAll('ul li a');
+// CURSOR HOVER EFFECT
 const largeCursor = document.getElementById('cursor');
 
-links.forEach(link => {
-    link.addEventListener('mouseenter', () => {
-        applyCursorEffect(largeCursor);
-    });
-
-    link.addEventListener('mouseleave', () => {
-        revertCursorEffect(largeCursor);
-    });
-});
-
-function applyCursorEffect(element) {
-    element.style.mixBlendMode = 'difference';
-    element.style.transform = 'scale(2)';
+function applyCursorEffect() {
+    largeCursor.style.mixBlendMode = 'difference';
+    largeCursor.style.transform = 'scale(3)';
 }
 
-function revertCursorEffect(element) {
-    element.style.mixBlendMode = 'normal';
-    element.style.width = '20px';
-    element.style.height = '20px';
-    element.style.transform = 'translate(-50%, -50%)';
+function revertCursorEffect() {
+    largeCursor.style.mixBlendMode = 'normal';
+    largeCursor.style.width = '20px';
+    largeCursor.style.height = '20px';
+    largeCursor.style.transform = 'translate(-50%, -50%)';
 }
 
 // MENU TOGGLER
+var isMenuVisible = false;
 var menuIcon = document.getElementById('menuIcon');
+var menuDisplay = document.getElementById('menuDisplay');
+var homeDisplay = document.getElementById('homeDisplay');
+
 menuIcon.innerHTML = `<svg class="ham hamRotate ham8" viewBox="0 0 100 100" width="80" onclick="this.classList.toggle('active')">
         <path class="line top"
             d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20" />
@@ -40,19 +34,30 @@ menuIcon.innerHTML = `<svg class="ham hamRotate ham8" viewBox="0 0 100 100" widt
             d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20" />
     </svg>`;
 
-var isShown = false;
 function displayMenu() {
-    var menuDisplay = document.getElementById('menuDisplay');
-    var homeDisplay = document.getElementById('homeDisplay');
 
-    if (!isShown) {
+    if (!isMenuVisible) {
         menuDisplay.style.display = 'block';
         homeDisplay.style.display = 'none';
-        console.log('menu shown');
     } else {
         menuDisplay.style.display = 'none';
         homeDisplay.style.display = 'block';
-        console.log('menu hidden');
     }
-    isShown = !isShown;
+    isMenuVisible = !isMenuVisible;
+}
+
+if (!isMenuVisible) {
+    menuDisplay.style.display = 'none';
+}
+
+function displayHomePage() {
+    isMenuVisible = false;
+    menuDisplay.style.display = 'none';
+    homeDisplay.style.display = 'block';
+
+    // RESET TOGGLER
+    const menuToggler = document.querySelector('.ham');
+    if (menuToggler.classList.contains('active')) {
+        menuToggler.classList.remove('active');
+    }
 }
